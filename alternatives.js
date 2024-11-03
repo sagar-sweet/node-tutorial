cf8cbba3299f3e66d8daba1c9f5ffab885bae76a
Sagar Chovatiya <chovatiyasgr@gmail.com>
2024-11-03 10:19:28 +0530

    first commit

---
+++ alternatives.js
@@ -0,0 +1,9 @@
+module.exports.items = ['item1','item2','item3','item4'];
+
+
+const person = {
+    name:"Peter",
+    age:25,
+    city:"surat"
+}
+module.exports.newPerson = person
---
+++ app.js
@@ -0,0 +1,14 @@
+
+// const names = require('./names')
+// const sayHi = require('./utils')
+
+const data = require('./alternatives')
+
+console.log(data.newPerson);
+
+
+
+
+
+// sayHi(names.john);
+// sayHi(names.peter);
